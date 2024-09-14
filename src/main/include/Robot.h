66d5678d85ac05de34835780e6c69b3bee0d41bd
vincent <25steelev@bishopdubourg.org>
2024-09-14 13:34:26 -0500

    Revert "yagsl-example"
    This reverts commit 72c6a2a8c4cf35ed5e9cf8fe1c95f0a43d91658e.

---
+++ src/main/include/Robot.h
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc/TimedRobot.h>
+#include <frc2/command/Command.h>
+#include <cameraserver/CameraServer.h>
+
+#include "RobotContainer.h"
+
+class Robot : public frc::TimedRobot {
+ public:
+  void DriveWithJoystick(bool fieldRelative);
+  void TeleopPeriodic() override { DriveWithJoystick(true); }
+  void AutonomousPeriodic() override;
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void AutonomousInit() override;
+  void TeleopInit() override;
+  void TestPeriodic() override;
+
+
+  void ConfigureButtonBindings();
+
+  frc::SlewRateLimiter<units::scalar> m_xspeedLimiter{3 / 1_s};
+  frc::SlewRateLimiter<units::scalar> m_yspeedLimiter{3 / 1_s};
+  frc::SlewRateLimiter<units::scalar> m_rotLimiter{3 / 1_s};
+
+  RobotContainer* m_container = RobotContainer::GetInstance();
+
+
+ private:
+  // Have it null by default so that if testing teleop it
+  // doesn't have undefined behavior and potentially crash.
+
+  // Have it null by default so that if testing teleop it
+  // doesn't have undefined behavior and potentially crash.
+  frc2::Command* m_autonomousCommand = nullptr;
+
+  //cs::UsbCamera usbcamera1;
+  //cs::UsbCamera usbcamera2;
+//
+  //cs::VideoSink cameraserver;
+};
---
+++ src/main/include/subsystems/Drivetrain.h
@@ -0,0 +1,82 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <numbers>
+
+#include <frc2/command/SubsystemBase.h>
+
+#include <frc/ADXRS450_Gyro.h>
+#include <frc/geometry/Translation2d.h>
+#include <frc/kinematics/SwerveDriveKinematics.h>
+#include <frc/kinematics/SwerveDriveOdometry.h>
+#include <rev/CANSparkMax.h>
+
+#include <frc/smartdashboard/SmartDashboard.h>
+
+#include "SwerveModule.h"
+
+#include "Constants.h"
+
+/**
+ * Represents a swerve drive style drivetrain.
+ */
+class Drivetrain: public frc2::SubsystemBase {
+ public:
+  Drivetrain();
+
+  void Drive(units::meters_per_second_t xSpeed,
+             units::meters_per_second_t ySpeed, units::radians_per_second_t rot,
+             bool fieldRelative, units::second_t period);
+  void Periodic() override;
+  void SimulationPeriodic() override;//TODO needed?
+  void UpdateOdometry();
+  void DriveStop();
+  void AutoDriveBackwards();
+  void AutoDriveForwards();
+  void AutoRotateLeft();
+  void AutoRotateRight();
+
+  static constexpr units::meters_per_second_t kMaxSpeed =
+      3.0_mps;  // 3 meters per second
+  static constexpr units::radians_per_second_t kMaxAngularSpeed{
+      std::numbers::pi};  // 1/2 rotation per second - todo ??? half?
+  
+  frc::ADXRS450_Gyro m_gyro;
+  
+  SwerveModule m_frontLeft{DriveConstants::kFrontLeftDriveMotorPort, rev::CANSparkMax::MotorType::kBrushless, DriveConstants::kFrontLeftTurningMotorPort, rev::CANSparkMax::MotorType::kBrushless, DriveConstants::kFrontLeftTurningEncoderPort, "rio"};
+  SwerveModule m_frontRight{DriveConstants::kFrontRightDriveMotorPort, rev::CANSparkMax::MotorType::kBrushless, DriveConstants::kFrontRightTurningMotorPort, rev::CANSparkMax::MotorType::kBrushless, DriveConstants::kFrontRightTurningEncoderPort, "rio"};
+  SwerveModule m_backLeft{DriveConstants::kBackLeftDriveMotorPort, rev::CANSparkMax::MotorType::kBrushless, DriveConstants::kBackLeftTurningMotorPort, rev::CANSparkMax::MotorType::kBrushless, DriveConstants::kBackLeftTurningEncoderPort, "rio"};
+  SwerveModule m_backRight{DriveConstants::kBackRightDriveMotorPort, rev::CANSparkMax::MotorType::kBrushless, DriveConstants::kBackRightTurningMotorPort, rev::CANSparkMax::MotorType::kBrushless, DriveConstants::kBackRightTurningEncoderPort, "rio"};
+
+ private:
+  frc::Translation2d m_frontLeftLocation{+DriveConstants::kWheelLocations, +DriveConstants::kWheelLocations};
+  frc::Translation2d m_frontRightLocation{+DriveConstants::kWheelLocations, -DriveConstants::kWheelLocations};
+  frc::Translation2d m_backLeftLocation{-DriveConstants::kWheelLocations, +DriveConstants::kWheelLocations};
+  frc::Translation2d m_backRightLocation{-DriveConstants::kWheelLocations, -DriveConstants::kWheelLocations};
+
+/*                         
+int driveMotorChannel
+rev::CANSparkMax::MotorType driveMotorType
+int turningMotorChannel
+rev::CANSparkMax::MotorType turningMotorType
+int turningEncoderChannel
+std::string turningEncoderCanbus
+*/
+
+
+
+
+
+  frc::SwerveDriveKinematics<4> m_kinematics{
+      m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation,
+      m_backRightLocation};
+
+  frc::SwerveDriveOdometry<4> m_odometry{
+      m_kinematics,
+      m_gyro.GetRotation2d(),
+      {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+       m_backLeft.GetPosition(), m_backRight.GetPosition()}};
+};
