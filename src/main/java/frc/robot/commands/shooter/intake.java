b28086fc50f20e9dec9c5fdb20666ea255e1245d
vincent <25steelev@bishopdubourg.org>
2025-01-25 10:58:23 -0600

    comment out test files

--- src/main/java/frc/robot/commands/shooter/intake.java
+++ src/main/java/frc/robot/commands/shooter/intake.java
@@ -5,17 +5,17 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.RobotContainer;
 
 public class intake extends Command{
-    public intake(){
-        addRequirements(RobotContainer.shooter);
-    }
-
-    public void execute() {
-        RobotContainer.shooter.intake();;
-    }
-
-    public void end(boolean interrupted) {
-        RobotContainer.shooter.intake();
-    }
-
+  //  public intake(){
+  //      addRequirements(RobotContainer.shooter);
+  //  }
+//
+  //  public void execute() {
+  //      RobotContainer.shooter.intake();;
+  //  }
+//
+  //  public void end(boolean interrupted) {
+  //      RobotContainer.shooter.intake();
+  //  }
+//
 
 }
--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -9,14 +9,14 @@ import frc.robot.RobotContainer;
 import frc.robot.Constants.ShooterConstants;
 public class shooter extends SubsystemBase {
   
-  SparkMax m_shooterMotor = new SparkMax(ShooterConstants.shooterMotorID, ShooterConstants.shooterMotorType);
-  double speed;
-
-    public void intake(){
-      m_shooterMotor.set(0.5);
-    }
-
-    public void shoot(){
-      m_shooterMotor.set(-0.5);
-    }
+ // SparkMax m_shooterMotor = new SparkMax(ShooterConstants.shooterMotorID, ShooterConstants.shooterMotorType);
+ // double speed;
+//
+ //   public void intake(){
+ //     m_shooterMotor.set(0.5);
+ //   }
+//
+ //   public void shoot(){
+ //     m_shooterMotor.set(-0.5);
+ //   }
    }
